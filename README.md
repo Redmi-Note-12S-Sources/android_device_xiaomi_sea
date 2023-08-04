43cfc5f434447e420408e7c7c702aeb8f8d2a442
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-08-04 23:31:24 +0000

    sea: Inital tree

---
+++ README.md
@@ -0,0 +1,25 @@
+# Device tree for Redmi Note 12S (codename: sea)
+
+Redmi Note 12S (codenamed _"sea"_) is a mid-range smartphone from Xiaomi.
+
+It was released in March 2023.
+
+## Known issues
+- TBA
+
+
+## Device specifications
+
+|                   Basic | Spec Sheet                                                         |
+| ----------------------: | :----------------------------------------------------------------- |
+|                     SoC | MediaTek® Helio G96 (MT6781)                                       |
+|                     CPU | Octa-core CPU with 2x Cortex-A76 & 6x Cortex-A55                   |
+|                     GPU | Mali-G57 MC2                                                       |
+|                  Memory | 6/8 RAM (LPDDR4X)                                                  |
+| Shipped Android Version | 11 with MIUI 13                                                    |
+|                 Storage | 64/128/256 (UFS 2.2)                                               |
+|                 Battery | 5000 mAh, non-removable                                            |
+|                 Display | 1080 x 2400 pixels, 20:9 ratio, 6.43 inches, 60/90 Hz, AMOLED      |
+|                  Camera | 108MP/64MP (Primary), 8MP (Ultra-wide), 2MP (Macro)                |
+
+![Redmi Note 12S](https://eustore.si/images/thumbnails/1086/800/detailed/73/res_95dd7a4046b56ab8dc10b53acee3.png)
---
+++ device.mk
@@ -0,0 +1,179 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/sea
+
+# Inherit virtual_ab_ota product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/xiaomi/sea/sea-vendor.mk)
+
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+    vbmeta \
+    vbmeta_system
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Dynamic partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# API level, the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# APN config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/apn/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
+# Bootctrl
+PRODUCT_PACKAGES += \
+    bootctrl.default \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2.recovery \
+    android.hardware.boot@1.2-service
+
+# DT2W
+PRODUCT_PACKAGES += \
+    DT2W-Service-Sea
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
+# FMRadio
+PRODUCT_PACKAGES += \
+    libfmjni \
+    FMRadio
+
+# ImsInit hack
+PRODUCT_PACKAGES += \
+    ImsInit
+
+# ImsService
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService \
+    ImsServiceBase
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mt6781.rc \
+    init.recovery.mt6781.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.mt6781.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.mt6781.rc
+
+# Health
+PRODUCT_PACKAGES += \
+    libsuspend \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhwbinder 
+
+# Kernel
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/dtb.img:dtb.img
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.sea
+
+# MTK In-call service
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+# Runtime Resource Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlaySea \
+    TetheringOverlaySea \
+    WifiOverlaySea \
+    SystemUIOverlaySea \
+    SettingsOverlaySea
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+ 
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6781:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6781
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_vtservice \
+    libshim_beanpod.vendor
+
+# Telephony
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+# Vendor overlay
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/vendor_overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+PRODUCT_EXTRA_VNDK_VERSIONS := 31
